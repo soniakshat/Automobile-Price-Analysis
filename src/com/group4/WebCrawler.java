84b49f6f54fb23b3e9a57879579125d0e69a639f
Akshat-MSI <akshat@spellenx.com>
2024-03-13 00:20:02 -0400

    Added WebCrawler Code

--- src/com/group4/WebCrawler.java
+++ src/com/group4/WebCrawler.java
@@ -1,9 +1,42 @@
 package com.group4;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
 /**
  *
  * Class to crawl web
  * @author Akshat Soni
  * */
 public class WebCrawler {
+
+    public static void main(String[] args) {
+        String[] urls = {"https://www.motorcitychrysler.ca/used/", "https://www.carpages.ca/ontario/windsor/used-cars/"};
+
+        for (String url : urls) {
+            fetchAndSaveHtml(url);
+        }
+    }
+
+    public static void fetchAndSaveHtml(String url) {
+        try {
+            Document document = Jsoup.connect(url).get();
+
+            String htmlCode = document.html();
+
+            String fileName = "html_output_" + System.currentTimeMillis() + ".txt";
+            try (FileWriter writer = new FileWriter(fileName)) {
+                writer.write(htmlCode);
+            }
+
+            System.out.println("HTML code from " + url + " saved to file: " + fileName);
+            System.out.println("\n------------------------------------------------\n");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
