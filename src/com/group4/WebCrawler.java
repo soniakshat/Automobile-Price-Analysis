940f7904dcdf9cfb9c33476834c9c9281d4bceb8
Akshat-MSI <akshat@spellenx.com>
2024-03-13 00:38:01 -0400

    Modified: WebCrawler to put the crawled files at proper place

--- src/com/group4/WebCrawler.java
+++ src/com/group4/WebCrawler.java
@@ -3,16 +3,19 @@ package com.group4;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
- *
  * Class to crawl web
+ *
  * @author Akshat Soni
- * */
+ */
 public class WebCrawler {
-
+    static final String folderPath = ".\\res\\pages\\";
     public static void main(String[] args) {
         String[] urls = {"https://www.motorcitychrysler.ca/used/", "https://www.carpages.ca/ontario/windsor/used-cars/"};
 
@@ -27,13 +30,22 @@ public class WebCrawler {
 
             String htmlCode = document.html();
 
-            String fileName = "html_output_" + System.currentTimeMillis() + ".txt";
-            try (FileWriter writer = new FileWriter(fileName)) {
-                writer.write(htmlCode);
+            File webPageFolder = new File(folderPath);
+
+            if(!webPageFolder.exists()){
+                Files.createDirectories(Paths.get(folderPath));
             }
 
-            System.out.println("HTML code from " + url + " saved to file: " + fileName);
-            System.out.println("\n------------------------------------------------\n");
+            if(webPageFolder.exists() && webPageFolder.isDirectory()){
+
+                String fileName = "html_output_" + System.currentTimeMillis() + ".txt";
+                try (FileWriter writer = new FileWriter(folderPath+fileName)) {
+                    writer.write(htmlCode);
+                }
+
+                CustomPrint.println("HTML code from " + url + " saved to file: " + fileName);
+                CustomPrint.println("\n------------------------------------------------\n");
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
